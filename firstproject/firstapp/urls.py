38f5ba756a4b94be1fa4da9d2e1cb863144fddfe
sabbir9847 <sabbirahmednayeem941@gmail.com>
2025-11-06 22:39:40 +0600

    changes
    sdf

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('function' , views.hellow_world),
     path('class' , views.HellowEthiopia.as_view()),
+    path('reservation' , views.home),
 ]
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservaionForm
 
 def hellow_world(request): 
     return HttpResponse("Hello world")
@@ -8,3 +9,15 @@ def hellow_world(request):
 class HellowEthiopia(View):
     def get(self ,  request):
         return HttpResponse("Hello Ethiopia")
+    
+
+def home(request):
+    form = ReservaionForm()
+
+    if request.method == 'POST':
+        form = ReservaionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("success")
+        
+        return render(request, 'index.html' , {'form' :  form})
